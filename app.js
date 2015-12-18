be73aa0ce72df6c27dee539a59a68dc069178e22
Jon Harlem <jtharlem@gmail.com>
2015-12-18 14:31:22 -0800

    added demo script

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-app.js
+var Nightmare = require('nightmare');
+
+var demo = new Nightmare({show: true})
+	//some sites have different versions for different browsers
+	.viewport(800, 800)
+	.useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490 Safari/537.36")
+	.goto('http://www.galvanize.com/')
+	.wait(1000)
+	//create a pdf
+	.screenshot('demo.png')
+	.run(function(err, nightmare) {
+		if (err) return console.log(err);
+		console.log('Done');
+	});
+
+demo.end();
